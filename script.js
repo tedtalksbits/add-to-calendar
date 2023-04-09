baa26336e43237fc2ddc3e9c1f3c5fd5153f9c29
tedtalksbits <tedaneblake@gmail.com>
2023-04-09 01:42:25 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let baseUrl = 'https://www.google.com/calendar/render?action=TEMPLATE';
+let eventName = document.getElementById('event-name');
+let eventDate1 = document.getElementById('event-date1');
+let eventDate2 = document.getElementById('event-date2');
+let eventLocation = document.getElementById('event-location');
+let eventDescription = document.getElementById('event-description');
+let submit = document.getElementById('submit');
+
+// set default date and time
+
+let today = new Date();
+
+eventDate1.value = today.toISOString().slice(0, 19);
+
+eventDate1.addEventListener('change', (e) => {
+    eventDate2.min = eventDate1.value;
+
+    if (eventDate2.value < eventDate1.value) {
+        eventDate2.value = eventDate1.value;
+    }
+});
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let formattedDate1 = formatDate(eventDate1.value);
+    let formattedDate2 = formatDate(eventDate2.value);
+
+    let url = `${baseUrl}&text=${eventName.value}&dates=${formattedDate1}/${formattedDate2}&location=${eventLocation.value}&details=${eventDescription.value}`;
+    window.open(url, '_blank');
+});
+
+function formatDate(date) {
+    let isoStringDate = new Date(date).toISOString();
+    // remove -, ., and :
+    let formattedDate = isoStringDate.replace(/-|\.|:/g, '');
+
+    return formattedDate;
+}
