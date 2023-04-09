bd18e99d113f106da40a86ec3b84ce308f912313
tedtalksbits <tedaneblake@gmail.com>
2023-04-09 01:47:39 -0400

    date validation

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ let submit = document.getElementById('submit');
 let today = new Date();
 
 eventDate1.value = today.toISOString().slice(0, 19);
+eventDate2.value = today.toISOString().slice(0, 19);
 
 eventDate1.addEventListener('change', (e) => {
     eventDate2.min = eventDate1.value;
@@ -20,17 +21,24 @@ eventDate1.addEventListener('change', (e) => {
     }
 });
 
+eventDate2.addEventListener('change', (e) => {
+    if (eventDate2.value < eventDate1.value) {
+        eventDate2.value = eventDate1.value;
+    }
+});
+
 submit.addEventListener('click', (e) => {
     e.preventDefault();
 
     let formattedDate1 = formatDate(eventDate1.value);
-    let formattedDate2 = formatDate(eventDate2.value);
+    let formattedDate2 = formatDate(eventDate2.value) || formattedDate1;
 
     let url = `${baseUrl}&text=${eventName.value}&dates=${formattedDate1}/${formattedDate2}&location=${eventLocation.value}&details=${eventDescription.value}`;
     window.open(url, '_blank');
 });
 
 function formatDate(date) {
+    if (!date) return;
     let isoStringDate = new Date(date).toISOString();
     // remove -, ., and :
     let formattedDate = isoStringDate.replace(/-|\.|:/g, '');
